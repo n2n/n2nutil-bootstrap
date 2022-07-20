28d5641658c052062cffff22c6e7721177628c0e
tomekk-hnm <jansen@hnm.ch>
2022-07-20 10:44:35 -0400

    Update BootstrapLibrary.php

--- src/app/n2nutil/bootstrap/ui/BootstrapLibrary.php
+++ src/app/n2nutil/bootstrap/ui/BootstrapLibrary.php
@@ -8,7 +8,7 @@ use n2nutil\jquery\JQueryLibrary;
 
 class BootstrapLibrary  extends LibraryAdapter {
 	// main and side version numbers are synchronized with the bootstrap version
-	const VERSION = '4.3.3';
+	const VERSION = '4.3.4';
 	
 	private $loadBootstrapCss;
 	private $loadJs;
