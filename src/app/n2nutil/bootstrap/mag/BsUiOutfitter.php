54b4fc31650e95baf8260f379f4deed0899e8e7d
Andreas von Burg <a@von-burg.net>
2024-12-04 00:09:51 +0100

    php 8.4 compatibility

--- src/app/n2nutil/bootstrap/mag/BsUiOutfitter.php
+++ src/app/n2nutil/bootstrap/mag/BsUiOutfitter.php
@@ -18,7 +18,7 @@ class BsUiOutfitter implements UiOutfitter {
 	private $controlAttrs;
 	private $checkControlAttrs;
 
-	public function __construct(OutfitConfig $outfitConfig = null, BsConfig $bsConfig = null,
+	public function __construct(?OutfitConfig $outfitConfig = null, ?BsConfig $bsConfig = null,
 			array $controlAttrs = array(), array $checkControlAttrs = array()) {
 
 		$this->outfitConfig = $outfitConfig;
@@ -65,7 +65,7 @@ class BsUiOutfitter implements UiOutfitter {
 	 * @param int $elemNature
 	 * @return HtmlElement
 	 */
-	public function createElement(int $elemNature, array $attrs = null, $contents = ''): UiComponent {
+	public function createElement(int $elemNature, ?array $attrs = null, $contents = ''): UiComponent {
 		if ($elemNature & self::EL_NATRUE_CONTROL_ADDON_SUFFIX_WRAPPER) {
 			$inputGroupAppend = new HtmlElement('span', array('class' => 'input-group-text'), $contents);
 			return new HtmlElement('div', HtmlUtils::mergeAttrs(array('class' => 'input-group'), $attrs), $inputGroupAppend);
@@ -120,7 +120,7 @@ class BsUiOutfitter implements UiOutfitter {
 		return new HtmlSnippet($contents);
 	}
 
-	public function createMagDispatchableView(PropertyPath $propertyPath = null, HtmlView $contextView): UiComponent {
+	public function createMagDispatchableView(?PropertyPath $propertyPath = null, HtmlView $contextView): UiComponent {
 		$bsChild = $this->bsConfig->getChild();
 		$bs = (null !== $bsChild) ? $bsChild : $this->bsConfig;
 		
